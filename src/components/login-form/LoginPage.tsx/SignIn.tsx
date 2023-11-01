5decb05a1c109ae5bb7f5838a21563b5baca1ce2
vitodent <140506272+vitodent@users.noreply.github.com>
2023-11-01 20:23:12 +0200

    craeted the sign in component

---
+++ src/components/login-form/LoginPage.tsx/SignIn.tsx
@@ -0,0 +1,91 @@
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { users } from "../../../data/users";
+import { useState } from "react";
+
+
+export const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const foundUser = users.find(
+      (user) => user.email === email && user.password === password
+    );
+    if (foundUser) {
+      console.log("user found");
+    } else {
+      console.log("user not found");
+    }
+  }
+
+  function handleChangeEmail(event: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(event.target.value.toLowerCase());
+  }
+
+  function handleChangePassword(event: React.ChangeEvent<HTMLInputElement>) {
+    setPassword(event.target.value.toLowerCase());
+  }
+
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={handleChangeEmail}
+            value={email}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={handleChangePassword}
+            value={password}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
---
+++ src/data/users.ts
@@ -0,0 +1,7 @@
+export const users = [{
+    email: 'test123@gmail.com',
+    password: '1234567',
+}, {
+    email: 'test000@gmail.com',
+    password: '7654321',
+}]
