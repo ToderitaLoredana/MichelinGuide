69305a0a557e17a26de1c5f1bb793ed726ecf925
ToderitaLoredana <loredanatoderita03@gmail.com>
2023-11-03 22:32:03 +0200

    created the first food card, created multiple cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Container } from "@mui/material";
 import Menu from "./components/menu/Menu";
+import FoodCard from "./components/food-card/FoodCard";
 
 const App = () => {
   return (
-      <Menu />
+      // <Menu />
+      <FoodCard />
   );
 };
 
