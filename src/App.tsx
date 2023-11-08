7e671f8f94d901d1f03a6376c5bd9137c77ca3cf
vitodent <140506272+vitodent@users.noreply.github.com>
2023-11-08 20:24:50 +0200

    was made a design for sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { Container } from "@mui/material";
-import Menu from "./components/menu/Menu";
-import {SignIn} from './components/login-form/LoginPage.tsx/SignIn';
+import {SignIn} from './components/sign-in-page/SignInPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SideMenu from "./components/menu/Menu";
 import ProtectedRoute from "./components/protected-route/ProtectedRoute";
